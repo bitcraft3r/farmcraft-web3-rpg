477bbff147509dbe418004186be94fc4e111c1d0
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-28 20:41:15 +0800

    chore: update chain to arbitrum goerli

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,18 +2,18 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 // import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ScrollAlphaTestnet } from "@thirdweb-dev/chains";
+// import { ScrollAlphaTestnet } from "@thirdweb-dev/chains";
 
 import NavBar from "../components/Navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "arbitrum-goerli";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={ScrollAlphaTestnet}>
+    <ThirdwebProvider activeChain={activeChain}>
       <ChakraProvider>
         <NavBar />
         <Component {...pageProps} />
