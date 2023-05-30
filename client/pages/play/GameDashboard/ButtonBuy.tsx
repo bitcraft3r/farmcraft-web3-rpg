4e9e3d273b0f3db972b4e46c1eacc98a44bfa115
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-30 17:36:44 +0800

    feat: call actual contract fns for Buy & Sell btns

--- client/pages/play/GameDashboard/ButtonBuy.tsx
+++ client/pages/play/GameDashboard/ButtonBuy.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useContractWrite } from 'wagmi'
+import CONTRACT_ABI from '../../../data/abi.json'
 
 const ButtonBuy = () => {
+  const farmerId = 1 // replace with actual farmerId
+
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    // @ts-ignore
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    abi: CONTRACT_ABI,
+    functionName: 'buySeeds',
+    args: [farmerId, 1],
+  })
+
+  const buyHandler = () => {
+    // @ts-ignore
+    write('buySeeds')
+  }
+
   return (
-    <button>Buy Seeds</button>
+    <button onClick={buyHandler} disabled={isLoading}>Buy Seeds</button>
   )
 }
 
--- client/pages/play/GameDashboard/ButtonSell.tsx
+++ client/pages/play/GameDashboard/ButtonSell.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useContractWrite } from 'wagmi'
+import CONTRACT_ABI from '../../../data/abi.json'
 
 const ButtonSell = () => {
+  const farmerId = 1 // replace with actual farmerId
+
+  const { data, isLoading, isSuccess, write } = useContractWrite({
+    // @ts-ignore
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    abi: CONTRACT_ABI,
+    functionName: 'sellCrops',
+    args: [farmerId, 10],
+  })
+
+  const sellHandler = () => {
+    // @ts-ignore
+    write('sellCrops')
+  }
+
   return (
-    <button>Sell Crops</button>
+    <button onClick={sellHandler} disabled={isLoading}>Sell Crops</button>
   )
 }
 
