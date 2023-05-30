0b746187ac3c6a7a021e4d78a2f1920304a21b3b
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-05-30 12:29:49 +0800

    feat: add farm minimap, change bg & text color

--- client/pages/game/GameDashboard.tsx
+++ client/pages/game/GameDashboard.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const GameDashboard = () => {
   return (
     <div>
-        You have a Farmer. Start playing!
+        <Image src="/images/minimap.webp" alt="FarmCraft Game Minimap" width={1200} height={800} />
     </div>
   )
 }
