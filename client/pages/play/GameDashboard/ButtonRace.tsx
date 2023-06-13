1b1d29cc7a69311dbfefefe59a1a6f87d9edf4f3
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-06-13 13:50:18 +0800

    feat: update race buttons based on store state

--- client/pages/play/GameDashboard/ButtonRace.tsx
+++ client/pages/play/GameDashboard/ButtonRace.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { useContractWrite } from 'wagmi'
+import React, { useEffect } from 'react'
+import { useContractRead, useContractWrite } from 'wagmi'
+
+import playerStore from "../../../store/contractStore";
 import CONTRACT_ABI from '../../../data/abi.json'
 import { Button } from '../../../components/ui/button'
 
@@ -8,8 +10,16 @@ interface ButtonRaceProps {
 }
 
 const ButtonRace: React.FC<ButtonRaceProps> = ({ farmerTokenId }) => {
+  const store = playerStore()
   const farmerId = farmerTokenId
   const wager = 1
+
+  const { data: dataCurrentRace, isError: isErrorCurrentRace, isLoading: isLoadingCurrentRace, refetch } = useContractRead({
+    // @ts-ignore
+    address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    abi: CONTRACT_ABI,
+    functionName: 'currentRace',
+  })
 
   const { data: dataRaceEnter, isLoading: isLoadingRaceEnter, isSuccess: isSuccessRaceEnter, write: writeRaceEnter } = useContractWrite({
     // @ts-ignore
@@ -34,6 +44,56 @@ const ButtonRace: React.FC<ButtonRaceProps> = ({ farmerTokenId }) => {
     args: [farmerId],
   })
 
+  useEffect(() => {
+    console.log(`dataCurrentRace`, dataCurrentRace)
+
+    if (isSuccessRaceEnter) {
+      console.log(`isSuccessRaceEnter`, isSuccessRaceEnter)
+      console.log(`dataRaceEnter:`, dataRaceEnter?.hash)
+
+      // Update store
+      store.setStatus(3)
+      store.increaseGold(-1)
+
+      // Refetch race data after 5s
+      console.log(`Refetching in 5s...`)
+      setTimeout(() => {
+        refetch()
+        console.log(`Done refetching!`)
+      }, 5000);
+    }
+
+    if (isSuccessRaceLeave) {
+      console.log(`isSuccessRaceLeave`, isSuccessRaceLeave)
+      console.log(`dataRaceLeave:`, dataRaceLeave?.hash)
+
+      // Update store
+      store.setStatus(0)
+      store.increaseGold(1)
+
+      // Refetch race data after 5s
+      console.log(`Refetching in 5s...`)
+      setTimeout(() => {
+        refetch()
+        console.log(`Done refetching!`)
+      }, 5000);
+    }
+
+    if (isSuccessRaceChallenge) {
+      console.log(`isSuccessRaceChallenge`, isSuccessRaceChallenge)
+      console.log(`dataRaceChallenge:`, dataRaceChallenge?.hash)
+
+      // REFRESH PAGE ON SUCCESSFUL CHALLENGE AFTER 5 SECONDS
+      if (dataRaceChallenge) {
+        console.log(`Refreshing in 5s...`)
+        setTimeout(() => {
+          window.location.reload();
+        }, 5000);
+      }
+    }
+
+  }, [dataCurrentRace, dataRaceEnter, isSuccessRaceEnter, dataRaceLeave, isSuccessRaceLeave, dataRaceChallenge, isSuccessRaceChallenge])
+
   const enterRace = () => {
     // @ts-ignore
     writeRaceEnter('enterRace')
@@ -51,9 +111,20 @@ const ButtonRace: React.FC<ButtonRaceProps> = ({ farmerTokenId }) => {
 
   return (
     <>
-      <Button variant="secondary" onClick={enterRace} disabled={isLoadingRaceEnter}>Enter Race</Button>
-      <Button variant="secondary" onClick={leaveRace} disabled={isLoadingRaceLeave}>Leave Race</Button>
-      <Button variant="secondary" onClick={challengeRace} disabled={isLoadingRaceChallenge}>Challenge Race</Button>
+      <Button variant="secondary" onClick={enterRace} disabled={isLoadingRaceEnter || store.status !== 0}>Enter Race</Button>
+      <Button variant="secondary" onClick={leaveRace} disabled={isLoadingRaceLeave || store.status !== 3}>Leave Race</Button>
+      <Button
+        variant="secondary"
+        onClick={challengeRace}
+        disabled={
+          isLoadingRaceChallenge ||
+          store.status !== 0 ||
+          // @ts-ignore
+          dataCurrentRace[2] === false
+        }
+      >
+        Challenge Race
+      </Button>
     </>
   )
 }
